b29fc4a30823d8ad559ac3134390db775c9c8c36
Tiago <tiagosilvapereira3@gmail.com>
2021-10-06 12:50:43 -0300

    Improvements

--- src/main.js
+++ src/main.js
@@ -12,6 +12,13 @@ module.exports = (vemto) => {
             })
         },
 
+        beforeCodeGeneration() {
+            let data = vemto.getPluginData()
+
+            vemto.log.info(data.text)
+            vemto.log.warning(`That's awesome!!! A Vemto plugin showing a message during code generation`)
+        },
+
     }
 
 }
